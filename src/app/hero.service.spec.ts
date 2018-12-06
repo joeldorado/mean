06635a7768a13bc7b797932c25d128c3d1e348ea
Joel Dorado aguilus <doradoaguilusjoel@gmail.com>
2018-12-05 20:33:33 -0800

    setup
    new mean app, nead mongo db

---
+++ src/app/hero.service.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HeroService } from './hero.service';
+
+describe('HeroService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [HeroService]
+    });
+  });
+
+  it('should be created', inject([HeroService], (service: HeroService) => {
+    expect(service).toBeTruthy();
+  }));
+
+
+  
+});
